d385f1c021f2909b8903f362b9c500093b8d3819
Josh B <josh.ja.butt@gmail.com>
2019-07-15 00:51:20 +1000

    Round down when displaying seconds.

--- js/display.js
+++ js/display.js
@@ -5,7 +5,7 @@ function seconds_to_string(seconds) {
     var numdays = Math.floor((seconds % 31536000) / 86400);
     var numhours = Math.floor(((seconds % 31536000) % 86400) / 3600);
     var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
-    var numseconds = (((seconds % 31536000) % 86400) % 3600) % 60;
+    var numseconds = Math.floor((((seconds % 31536000) % 86400) % 3600) % 60);
     return numdays + "d " + numhours + "h " + numminutes + "m " + numseconds + "s";
 }
 
